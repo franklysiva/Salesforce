15e846cc7fdddc4f604feb6b8c39077cd6fcc82d
Siva <sivakumar.s.92@gmail.com>
2022-07-21 10:28:35 +0530

    Update EditOpportunity.java

--- Salesforce/usingMainMethod/EditOpportunity.java
+++ Salesforce/usingMainMethod/EditOpportunity.java
@@ -23,20 +23,18 @@ public class EditOpportunity {
 	driver.findElement(By.id("username")).sendKeys("hari.radhakrishnan@qeagle.com");
 	driver.findElement(By.id("password")).sendKeys("Tuna@123");
 	driver.findElement(By.id("Login")).click();
-	new WebDriverWait(driver,  Duration.ofMinutes(1))
-			.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='slds-icon-waffle']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-			.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='slds-button']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-			.until(ExpectedConditions.elementToBeClickable(By.xpath("//p[text()='Sales']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-		.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Opportunities']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-		.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='Opportunity-search-input']")))
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(1));
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='slds-icon-waffle']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='slds-button']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//p[text()='Sales']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Opportunities']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='Opportunity-search-input']")))
 		.sendKeys("Salesforce Automation by Siva", Keys.ENTER);
 	Thread.sleep(5000);
 	WebElement showMoreElement = driver.findElement(By.xpath("(//tr//td//a[@role='button'])[1]"));
 	driver.executeScript("arguments[0].click()", showMoreElement);
+	String title = driver.findElement(By.xpath("(//tbody//tr[1]//a)[1]")).getAttribute("title");
+	if (title.equalsIgnoreCase("Salesforce Automation by Siva")) {
 	Thread.sleep(2000);
 	driver.findElement(By.xpath("//div[contains(@class,'actionMenu')]//li[@class='uiMenuItem']//a[@title='Edit']")).click();
 	Thread.sleep(5000);
@@ -45,23 +43,21 @@ public class EditOpportunity {
 	closeDateElement.sendKeys("7/21/2022");
 	WebElement stageElement = driver.findElement(By.xpath("(//label[text()='Stage']//following::button)[1]"));
 	stageElement.click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-	.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//span[@class='slds-media__body']")));
-	new WebDriverWait(driver,  Duration.ofMinutes(1))
-	.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[@class='slds-media__body'])[7]"))).click();
+	wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//span[@class='slds-media__body']")));
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[@class='slds-media__body'])[7]"))).click();
 	Thread.sleep(2000);
 	WebElement deliveryElement = driver.findElement(By.xpath("//label[contains(text(),'Installation Status')]//following::div//span[@class='slds-truncate']"));
 	driver.executeScript("arguments[0].scrollIntoView", deliveryElement);
 	Thread.sleep(2000);
 	driver.executeScript("arguments[0].click()", deliveryElement);
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-		.until(ExpectedConditions.elementToBeClickable(By.xpath("(//label[contains(text(),'Installation Status')]//following::div//span[@class='slds-media__body'])[2]"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//label[contains(text(),'Installation Status')]//following::div//span[@class='slds-media__body'])[2]"))).click();
 	WebElement descElement = driver.findElement(By.xpath("//label[text()='Description']//following::textarea"));
 	driver.executeScript("arguments[0].scrollIntoView", descElement);
 	descElement.sendKeys("SalesForce");
 	driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
 	Thread.sleep(5000);
-	String currentStage = driver.findElement(By.xpath("//div[@class='slds-path']//a[@aria-checked='true']")).getAttribute("title");
+	String currentStage = driver.findElement(By.xpath("(//table//tr[1]/td)[5]")).getText();
 	System.out.println("Stage Verified as: \""+currentStage+"\"");
+		}
 	}
 }
