8e75ce9d4c016eb1934d9a1f4be9830a48ca802f
Siva <sivakumar.s.92@gmail.com>
2022-07-21 10:28:03 +0530

    Update CreateNewOppurtunity.java

--- Salesforce/usingMainMethod/CreateNewOppurtunity.java
+++ Salesforce/usingMainMethod/CreateNewOppurtunity.java
@@ -22,30 +22,22 @@ public class CreateNewOppurtunity {
 	driver.findElement(By.id("username")).sendKeys("hari.radhakrishnan@qeagle.com");
 	driver.findElement(By.id("password")).sendKeys("Tuna@123");
 	driver.findElement(By.id("Login")).click();
-	new WebDriverWait(driver,  Duration.ofMinutes(1))
-			.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='slds-icon-waffle']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-			.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='slds-button']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-			.until(ExpectedConditions.elementToBeClickable(By.xpath("//p[text()='Sales']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-		.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Opportunities']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-		.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@title='New']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-		.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[text()='Opportunity Name']//following::input")))
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(1));
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='slds-icon-waffle']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='slds-button']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//p[text()='Sales']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Opportunities']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@title='New']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[text()='Opportunity Name']//following::input")))
 		.sendKeys("Salesforce Automation by Siva");
 	WebElement closeDateElement = driver.findElement(By.xpath("//label[text()='Close Date']//following::input"));
 	closeDateElement.sendKeys("7/20/2022");
 	WebElement stageElement = driver.findElement(By.xpath("(//label[text()='Stage']//following::button)[1]"));
 	stageElement.click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-		.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//span[@class='slds-media__body']")));
-	new WebDriverWait(driver,  Duration.ofMinutes(1))
-		.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[@class='slds-media__body'])[4]"))).click();
+	wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//span[@class='slds-media__body']")));
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[@class='slds-media__body'])[4]"))).click();
 	driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
-	WebElement createdName = new WebDriverWait(driver, Duration.ofMinutes(1))
-			.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1//slot")));
+	WebElement createdName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1//slot")));
 	String title = driver.findElement(By.xpath("//span[contains(@class,'toastMessage')]//a")).getAttribute("title");
 	if (createdName.getText().equalsIgnoreCase(title))
 		System.out.println("Verified Opportunity Name");
