74ebe135f0d6d79018ffd2fc23a0d2f2afa5854e
Siva <sivakumar.s.92@gmail.com>
2022-07-21 10:29:02 +0530

    Update DeleteOpportunity.java

--- Salesforce/usingMainMethod/DeleteOpportunity.java
+++ Salesforce/usingMainMethod/DeleteOpportunity.java
@@ -23,17 +23,15 @@ public class DeleteOpportunity {
 	driver.findElement(By.id("username")).sendKeys("hari.radhakrishnan@qeagle.com");
 	driver.findElement(By.id("password")).sendKeys("Tuna@123");
 	driver.findElement(By.id("Login")).click();
-	new WebDriverWait(driver,  Duration.ofMinutes(1))
-			.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='slds-icon-waffle']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-			.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='slds-button']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-			.until(ExpectedConditions.elementToBeClickable(By.xpath("//p[text()='Sales']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-		.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Opportunities']"))).click();
-	new WebDriverWait(driver, Duration.ofMinutes(1))
-		.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='Opportunity-search-input']")))
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(1));
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='slds-icon-waffle']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='slds-button']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//p[text()='Sales']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Opportunities']"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='Opportunity-search-input']")))
 		.sendKeys("Salesforce Automation by Siva", Keys.ENTER);
+	String title = driver.findElement(By.xpath("(//tbody//tr[1]//a)[1]")).getAttribute("title");
+	if (title.equalsIgnoreCase("Salesforce Automation by Siva")) {
 	Thread.sleep(2000);
 	WebElement showMoreElement = driver.findElement(By.xpath("(//tr//td//a[@role='button'])[1]"));
 	driver.executeScript("arguments[0].click()", showMoreElement);
@@ -41,8 +39,9 @@ public class DeleteOpportunity {
 	driver.findElement(By.xpath("//div[contains(@class,'actionMenu')]//li[@class='uiMenuItem']//a[@title='Delete']")).click();
 	Thread.sleep(2000);
 	driver.findElement(By.xpath("//span[text()='Delete']")).click();
-	String toastMsg = new WebDriverWait(driver, Duration.ofSeconds(30))
-		.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='forceActionLink']//parent::span"))).getText();
+	String toastMsg = wait
+			.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='forceActionLink']//parent::span"))).getText();
 	System.out.println(toastMsg.substring(0,toastMsg.length()-5));
+		}
 	}
 }
