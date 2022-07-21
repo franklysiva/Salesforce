fe357f62b6924f51b8e5c18d78ea78ddbce47532
Siva <sivakumar.s.92@gmail.com>
2022-07-21 10:31:34 +0530

    Update OpportunityWithoutMandatory.java

--- Salesforce/usingMainMethod/OpportunityWithoutMandatory.java
+++ Salesforce/usingMainMethod/OpportunityWithoutMandatory.java
@@ -23,16 +23,12 @@ public class OpportunityWithoutMandatory {
 		driver.findElement(By.id("username")).sendKeys("hari.radhakrishnan@qeagle.com");
 		driver.findElement(By.id("password")).sendKeys("Tuna@123");
 		driver.findElement(By.id("Login")).click();
-		new WebDriverWait(driver,  Duration.ofMinutes(1))
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='slds-icon-waffle']"))).click();
-		new WebDriverWait(driver, Duration.ofMinutes(1))
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='slds-button']"))).click();
-		new WebDriverWait(driver, Duration.ofMinutes(1))
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//p[text()='Sales']"))).click();
-		new WebDriverWait(driver, Duration.ofMinutes(1))
-			.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Opportunities']"))).click();
-		new WebDriverWait(driver, Duration.ofMinutes(1))
-			.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@title='New']"))).click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(1));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='slds-icon-waffle']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='slds-button']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//p[text()='Sales']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Opportunities']"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@title='New']"))).click();
 		Thread.sleep(5000);
 		WebElement closeDateElement = driver.findElement(By.xpath("//label[text()='Close Date']//following::input"));
 		closeDateElement.sendKeys("7/22/2022");
